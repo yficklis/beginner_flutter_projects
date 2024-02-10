a08d8ea175ac966b95d7a57fa5b72a0de6a9a063
yficklis <yficklis.santos@gmail.com>
2024-02-10 00:37:23 -0300

    COMPARISON OPERATORS AND LOGICAL OPERATORS WITH MATH

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,40 @@ class MyApp extends StatelessWidget {
   double myHeight = 1.78;
   bool isBeginner = true;
 
+  /*
+  BASIC WAY TO SHOW SOMETHING:
+  print("hi there");
+
+  BASIC MATH OPERATORS:
+  1 + 1 -> 2, add
+  4 - 1 -> 3, subtract
+  2 * 3 -> 6, multiply
+  8 / 4 -> 2, divide
+  9 % 4 -> 1, remainder
+
+  COMPARISON OPERATORS:
+  5 == 5 -> true, EQUAL TO
+  2 != 3 -> true, NOT EQUAL TO
+  3 > 2 -> true, GREATER THAN
+  3 < 2 -> false, LESS THAN
+  5 >= 5 -> true, GREATER OR EQUAL TO
+  3 <= 7 -> true, LESS THAN OR EQUAL TO
+
+  LOGICAL OPERATORS:
+  AND operator, returns true if both sides are true
+  isBeginner && (age < 18) -> return true
+
+  OR operator, returns true if at least one side is true
+  isBeginner || (age < 18) -> return true
+
+  NOT operator, returns the oppsite value
+  !isBeginner -> return false
+  
+  */
+
   @override
   Widget build(BuildContext context) {
+    print(isBeginner || (age < 18));
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
