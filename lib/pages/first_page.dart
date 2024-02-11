64c6dcccbcd22a50016aee728e81b21f14f5bc91
yficklis <yficklis.santos@gmail.com>
2024-02-11 02:03:39 -0300

    NAVIGATOR WITH PUSH

---
+++ lib/pages/first_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:beginner_course/pages/second_page.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("1st Page"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // navigate to second page
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondPage(),
+              ),
+            );
+          },
+          child: Text("Go To Second Page"),
+        ),
+      ),
+    );
+  }
+}
