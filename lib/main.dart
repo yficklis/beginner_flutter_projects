95c88114fa94ee23d711263c7f241f12494d502f
yficklis <yficklis.santos@gmail.com>
2024-02-11 01:11:02 -0300

    STACK WIDGET

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GridView.builder(
-          itemCount: 64,
-          gridDelegate:
-              // how many in each row
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-          itemBuilder: (context, index) => Container(
-            color: Colors.deepPurple,
-            margin: EdgeInsets.all(2),
-          ),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            // big box
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.deepPurple,
+            ),
+
+            // medium box
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[400],
+            ),
+
+            // small box
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurple[200],
+            ),
+          ],
         ),
       ),
     );
