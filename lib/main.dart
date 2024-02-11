d2b601beba329751ef9da446d029f11292d9665e
yficklis <yficklis.santos@gmail.com>
2024-02-11 01:30:37 -0300

    NAVIGATION ROUTES

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:beginner_course/pages/first_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  List names = ['Yficklis', 'Julia', "Thay Lung", 'Mingau'];
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: () => {
-              // do something, when user tapped the container.
-              print("User tapped!");
-            },
-            child: Container(
-              height: 200,
-              width: 200,
-              color: Colors.deepPurple[400],
-              child: Center(
-                child: Text("Tap me!"),
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: FirstPage(),
     );
   }
 }
